9eee1ac664b34d193ec99c0ab352e7ff33b1e709
Car Jeff <49448671+carjeff@users.noreply.github.com>
2019-11-18 10:57:20 +0800

    add the queue template and the queue test

---
+++ QueueTest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"Queue.h"
+using namespace std;
+
+int main(){
+    Queue<int> a(10);
+    a.In(1);
+    a.In(4);
+    a.Print();
+    a.In(5);
+    a.In(100);
+    a.In(120);
+    a.Print();
+    a.Out();
+    a.Print();
+
+    return 0;
+}
--- queue.h
+++ queue.h
@@ -4,15 +4,62 @@ using namespace std;
 template<typename type>
 class Queue{
     private:
-        int front;//定义头指针
-        int rear;//定义尾指针
+        int head;//定义头指针
+        int tail;//定义尾指针
         type *elements;
         int maxsize;
     public:
-        Queue(int n):top(0),rear(0),maxsize(n){
+        Queue(int n):head(0),tail(0),maxsize(n){
             elements = new type[maxsize];
             if(elements == NULL){
                 cout<<"Memory allocation failed"<<endl;
+                exit(-1);
             }
         }
+        ~Queue(){
+            delete[] elements;
+        }
+        void In(type item);//进队列
+        type Out();//出队列
+        bool IsEmpty(){ //判断队空
+            return head == tail;
+        }
+        bool IsFull(){ //判断队满
+            return (tail+1)%maxsize == maxsize;
+        }
+        void Print();
+};
+
+
+template<typename type> void Queue<type>::In(type item){
+    if(IsFull()){ //队尾入
+        cout<<"the queue is full!"<<endl;
+    }
+    else
+    {
+        elements[tail]=item;
+        tail = (tail+1)%maxsize;
+    }
+    
+}
+
+template<typename type> type Queue<type>::Out(){
+    if(IsEmpty()){ //队头出
+        cout<<"the queue is empty"<<endl;
+    }
+    else
+    {
+        type temp;
+        temp = elements[head];
+        head = (head+1)%maxsize;
+    }
+    
+}
+
+template<typename type> void Queue<type>::Print(){
+    for (int i = head; i<tail; i++)
+    {
+        cout<<elements[i]<<" ";
+    }
+    cout<<endl;
 }
